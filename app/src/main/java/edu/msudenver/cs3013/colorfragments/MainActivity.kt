813d267402ac255b136a43edc299b0d1cf8b4f42
konkelmp <preston.konkel@gmail.com>
2025-04-22 22:34:19 -0600

    Finalize

--- app/src/main/java/edu/msudenver/cs3013/colorfragments/MainActivity.kt
+++ app/src/main/java/edu/msudenver/cs3013/colorfragments/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity(), ChoiceListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
